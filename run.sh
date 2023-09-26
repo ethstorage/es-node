f0674c2876c271b2d8a73235ea17ccc01739f85b
syntrust <lundeng@quarkchain.org>
2023-09-26 16:22:29 +0800

    fixes

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# to be compatible with docker
-cd ../es-node
+miner="<miner>"
+private_key="<private_key>"
+
+if [ "$miner" = "<miner>" ]; then
+  echo "Please replace <miner> with your own"
+  exit 1
+fi
+if [ "$private_key" = "<private_key>" ]; then
+  echo "Please replace <private_key> with your own"
+  exit 1
+fi
 
 # download blob_poseidon.zkey if not yet
 zkey_file="./ethstorage/prover/snarkjs/blob_poseidon.zkey"
@@ -13,7 +22,9 @@ if [ ! -e  ${zkey_file} ]; then
   echo "downloaded ${zkey_file}"
 fi
 
-private_key="95eb6ffd2ae0b115db4d1f0d58388216f9d026896696a5211d77b5f14eb5badf"
+# to be compatible with docker
+cd ../es-node
+
 executable="./cmd/es-node/es-node"
 data_dir="./es-data"
 storage_file_0="$data_dir/shard-0.dat"
@@ -21,7 +32,7 @@ storage_file_0="$data_dir/shard-0.dat"
 common_flags=" --datadir $data_dir \
   --l1.rpc http://65.108.236.27:8545 \
   --storage.l1contract 0xF83c395c1e0e261578D6732ac277404eeb2f99eA \
-  --storage.miner 0xdF8466f277964Bb7a0FFD819403302C34DCD530A"
+  --storage.miner $miner"
 
 # init shard 0
 es_node_init="init --shard_index 0"
@@ -37,12 +48,12 @@ es_node_start=" --network devnet \
   --l1.beacon-based-time 1693820652 \
   --l1.beacon-based-slot 136521 \
   --p2p.listen.udp 30305  \
-  --p2p.bootnodes enr:-LC4QCABjeNlNMLy2Ai7Z9b0lbZAlno64zk5kEHM7sXLFGhwaalCLCcZ7BlrpDdWQRZlcROYXo8fO2WqBIUjsBGPYB-GAYrL92ALimV0aHN0b3JhZ2XbAYDY15T4PDlcHg4mFXjWcyrCd0BO6y-Z6sGAgmlkgnY0iXNlY3AyNTZrMaEDba07XjDdaXyvIQ0DmN-cvhOY5yAZRu4frQNgtKhI9iKDdGNwgiQGg3VkcIJ2YQ id=e770b1ddfe8e9238 \
+  --p2p.bootnodes enr:-LC4QCABjeNlNMLy2Ai7Z9b0lbZAlno64zk5kEHM7sXLFGhwaalCLCcZ7BlrpDdWQRZlcROYXo8fO2WqBIUjsBGPYB-GAYrL92ALimV0aHN0b3JhZ2XbAYDY15T4PDlcHg4mFXjWcyrCd0BO6y-Z6sGAgmlkgnY0iXNlY3AyNTZrMaEDba07XjDdaXyvIQ0DmN-cvhOY5yAZRu4frQNgtKhI9iKDdGNwgiQGg3VkcIJ2YQ \
   "
 # create data file for shard 0 if not yet
 if [ ! -e $storage_file_0 ]; then
   $executable $es_node_init $common_flags
-   echo "initialized $storage_file_0"
+  echo "initialized ${storage_file_0}"
 fi
 
 # start es-node
