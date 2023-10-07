916a01f1b815b67fd87c981a1121f0deeeef7ad0
syntrust <lundeng@quarkchain.org>
2023-10-07 08:34:49 +0000

    docker logs

--- run-docker.sh
+++ run-docker.sh
@@ -3,15 +3,14 @@
 # usage:
 # env ES_NODE_STORAGE_MINER=<miner> ES_NODE_PRIVATE_KEY=<private_key> ./run-docker.sh
 
-log_name="es-docker.log"
 container_name="es"
 image_name="es-node"
 
 if docker ps -a --format "{{.Names}}" | grep -q "^$container_name$"; then
-    docker start $container_name -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY  >> $log_name
+    docker start $container_name  
 else
     if ! docker images | grep -q "^$image_name$"; then
         docker build -t $image_name .
     fi
-    docker run --name $container_name -v ./es-data:/es-node/es-data -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY -it --entrypoint /es-node/run.sh $image_name >> $log_name
+    docker run --name $container_name -v ./es-data:/es-node/es-data -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY -d --entrypoint /es-node/run.sh $image_name 
 fi
