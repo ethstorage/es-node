9ed43dc5c9f99ffd33a64f1bf773f4fcc026fd89
syntrust <lundeng@quarkchain.org>
2023-10-07 15:57:33 +0800

    rename

--- run-docker.sh
+++ run-docker.sh
@@ -5,11 +5,12 @@
 
 log_name="es-docker.log"
 container_name="es"
+image_name="es-node"
+
 if docker ps -a --format "{{.Names}}" | grep -q "^$container_name$"; then
     docker -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY start $container_name >> $log_name
 else
-    image_name="es-node"
-    if ! docker image_names | grep -q $image_name; then
+    if ! docker images | grep -q "^$image_name$"; then
         docker build -t $image_name .
     fi
     docker run --name $container_name -v ./es-data:/es-node/es-data -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY -it --entrypoint /es-node/run.sh $image_name >> $log_name
