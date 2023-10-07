261ebe064881add9a49bf333844ea090e8ef58a4
syntrust <lundeng@quarkchain.org>
2023-10-07 15:50:35 +0800

    run docker

--- run-docker.sh
+++ run-docker.sh
@@ -3,13 +3,14 @@
 # usage:
 # env ES_NODE_STORAGE_MINER=<miner> ES_NODE_PRIVATE_KEY=<private_key> ./run-docker.sh
 
-container="es"
-if [[ $(docker ps -aq -f name=$container) ]]; then
-    docker -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY start $container >> es-docker.log
+log_name="es-docker.log"
+container_name="es"
+if docker ps -a --format "{{.Names}}" | grep -q "^$container_name$"; then
+    docker -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY start $container_name >> $log_name
 else
-    image="es-node"
-    if ! docker images | grep -q $image; then
-        docker build -t $image .
+    image_name="es-node"
+    if ! docker image_names | grep -q $image_name; then
+        docker build -t $image_name .
     fi
-    docker run -v ./es-data:/es-node/es-data -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY -it --entrypoint /es-node/run.sh $image >> es-docker.log
+    docker run --name $container_name -v ./es-data:/es-node/es-data -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER -e ES_NODE_PRIVATE_KEY=$ES_NODE_PRIVATE_KEY -it --entrypoint /es-node/run.sh $image_name >> $log_name
 fi
