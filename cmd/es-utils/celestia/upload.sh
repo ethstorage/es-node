74fd4a859bcb00f66e73075a8d5b0a4837a2b891
syntrust <lundeng@quarkchain.org>
2023-11-02 14:27:25 +0800

    update sh

--- cmd/es-utils/celestia/upload.sh
+++ cmd/es-utils/celestia/upload.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# usage:
-# env ES_NODE_SIGNER_PRIVATE_KEY=<private_key> ./upload.sh
-
 if [ -z "$ES_NODE_SIGNER_PRIVATE_KEY" ]; then
   echo "Please provide 'ES_NODE_SIGNER_PRIVATE_KEY' as environment variable"
+  echo "Usage:
+  env ES_NODE_SIGNER_PRIVATE_KEY=<private_key> ./upload.sh"
   exit 1
 fi
 
-./elestia upload --blob-file blob.dat \
+./celestia upload --blob-file blob.dat \
   --da-rpc http://65.108.236.27:26658 \
   --namespace-id 00000000000000003333 \
   --auth-token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJwdWJsaWMiLCJyZWFkIiwid3JpdGUiLCJhZG1pbiJdfQ.MaHtzm_HvBw810jMsd1Vr4bz1f4oAMPZExRNsOJ9n1g \
