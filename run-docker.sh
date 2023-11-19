bb1fda4444ff92a40c2f4ee6977abfaffb0be129
syntrust <lundeng@quarkchain.org>
2023-11-19 12:14:53 +0800

    sudo

--- run-docker.sh
+++ run-docker.sh
@@ -27,21 +27,21 @@ container_name="es"
 image_name="es-node" 
 
 # check if container is running
-if docker ps --format "{{.Names}}" | grep -q "^$container_name$"; then
+if sudo docker ps --format "{{.Names}}" | grep -q "^$container_name$"; then
     echo "container $container_name already started"
 else
     # start container if exist
-    if docker ps -a --format "{{.Names}}" | grep -q "^$container_name$"; then
-        docker start $container_name
+    if sudo docker ps -a --format "{{.Names}}" | grep -q "^$container_name$"; then
+        sudo docker start $container_name
         echo "container $container_name started"
     else
         # build an image if not exist
-        if ! docker images --format "{{.Repository}}" | grep -q "^$image_name$"; then
-            docker build -t $image_name .
+        if ! sudo docker images --format "{{.Repository}}" | grep -q "^$image_name$"; then
+            sudo docker build -t $image_name .
             echo "image $image_name built"
         fi
         # run container in the background
-       docker run --name $container_name \
+       sudo docker run --name $container_name \
             -v ./es-data:/es-node/es-data \
             -e ES_NODE_STORAGE_MINER=$ES_NODE_STORAGE_MINER \
             -e ES_NODE_SIGNER_PRIVATE_KEY=$ES_NODE_SIGNER_PRIVATE_KEY \
