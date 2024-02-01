7b6069cf1b243d2530476ac30801a6b3932e3e6e
pingke <pingke@quarkchain.org>
2024-02-01 15:58:46 +0800

    refact change

--- ethstorage/node/node.go
+++ ethstorage/node/node.go
@@ -380,8 +380,9 @@ func (n *EsNode) Close() error {
 	return result.ErrorOrNil()
 }
 
-func (n *EsNode) initDatabase(cfg *Config) (err error) {
+func (n *EsNode) initDatabase(cfg *Config) error {
 	var db ethdb.Database
+	var err error
 	if cfg.DataDir == "" {
 		db = rawdb.NewMemoryDatabase()
 	} else {
@@ -398,5 +399,5 @@ func (n *EsNode) initDatabase(cfg *Config) (err error) {
 	if err == nil {
 		n.db = db
 	}
-	return nil
+	return err
 }
