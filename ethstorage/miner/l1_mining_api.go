765ac65f152fae4f6d8215f19f65fd46bcbdc842
syntrust <lundeng@quarkchain.org>
2024-03-08 21:27:56 -0800

    stats more err

--- ethstorage/miner/l1_mining_api.go
+++ ethstorage/miner/l1_mining_api.go
@@ -170,7 +170,7 @@ func (m *l1MiningAPI) SubmitMinedResult(ctx context.Context, contract common.Add
 			"profitEstimated", weiToEther(profit),
 			"minimumProfit", weiToEther(cfg.MinimumProfit),
 		)
-		return common.Hash{}, fmt.Errorf("dropped: not enough profit")
+		return common.Hash{}, nil
 	}
 
 	chainID, err := m.NetworkID(ctx)
