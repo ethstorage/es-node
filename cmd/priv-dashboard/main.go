3a761124f539baebeb6d34a2531c155b5d2145c5
pingke <pingke@quarkchain.org>
2024-04-07 15:15:11 +0800

    add log for private dashboard

--- cmd/priv-dashboard/main.go
+++ cmd/priv-dashboard/main.go
@@ -84,6 +84,7 @@ func (d *dashboard) ReportStateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Info("Get state from peer", "peer id", state.Id)
 	d.nodes[state.Id] = &record{receivedTime: time.Now(), state: &state}
 	for _, shard := range state.Shards {
 		d.m.SetPeerInfo(state.Id, state.Version, state.Address, shard.ShardId, shard.Miner)
