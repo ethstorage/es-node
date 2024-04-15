b4e85007a7287a9cad7a94e48bf670e879eb5a3a
syntrust <lundeng@quarkchain.org>
2024-04-15 19:48:26 +0800

    test

--- integration_tests/archiver_test.go
+++ integration_tests/archiver_test.go
@@ -14,26 +14,66 @@ import (
 
 const (
 	urlPattern   = "http://%s/eth/v1/beacon/blob_sidecars/%s"
-	beaconAddr   = "88.99.30.186:3500"
 	archiverAddr = "65.108.236.27:9645"
+	beaconAddr   = "88.99.30.186:3500"
 )
 
-func TestQueryUrls(t *testing.T) {
-	tests := []string{
-		"4756895?indices=1",
+func TestArchiveAPI(t *testing.T) {
+
+	tests := []struct {
+		query  string
+		blobsa int
+		blobsb int
+		err    error
+	}{
+		{
+			query:  "4756895?indices=1",
+			blobsa: 0,
+			blobsb: 0,
+			err:    nil,
+		},
+		{
+			query:  "4756895?indices=2",
+			blobsa: 0,
+			blobsb: 1,
+			err:    nil,
+		},
 	}
 
-	for i, _ := range tests {
-		url := fmt.Sprintf(urlPattern, beaconAddr, tests[i])
-		res, err := makeQuery(t, url)
+	for _, tt := range tests {
+		urla := fmt.Sprintf(urlPattern, archiverAddr, tt.query)
+		resa, err := makeQuery(t, urla)
 		if err != nil {
-			t.Fatalf("Failed to query URL %s: %v", url, err)
+			t.Fatalf("Failed to query URL %s: %v", urla, err)
 		}
-		for _, d := range res.Data {
-			t.Logf("%+v\n", d)
+		urlb := fmt.Sprintf(urlPattern, beaconAddr, tt.query)
+		resb, err := makeQuery(t, urlb)
+		if err != nil {
+			t.Fatalf("Failed to query URL %s: %v", urlb, err)
 		}
-
-		t.Logf("Successfully queried and verified data from URL: %s\n", url)
+		t.Log("resb", len(resb.Data))
+		if len(resa.Data) != tt.blobsa {
+			t.Errorf("Expected %d blobs, got %d", tt.blobsa, len(resa.Data))
+			continue
+		}
+		blobs := tt.blobsb
+		if blobs > tt.blobsa {
+			blobs = tt.blobsa
+		}
+		for i := 0; i < blobs; i++ {
+			if resa.Data[i].Index != resb.Data[i].Index {
+				t.Errorf("Index does not match: %d != %d", resa.Data[i].Index, resb.Data[i].Index)
+			}
+			if resa.Data[i].KZGCommitment != resb.Data[i].KZGCommitment {
+				t.Errorf("KZGCommitment does not match: %s != %s", resa.Data[i].KZGCommitment, resb.Data[i].KZGCommitment)
+			}
+			if resa.Data[i].KZGProof != resb.Data[i].KZGProof {
+				t.Errorf("KZGProof does not match: %s != %s", resa.Data[i].KZGProof, resb.Data[i].KZGProof)
+			}
+			if resa.Data[i].Blob != resb.Data[i].Blob {
+				t.Errorf("Blob does not match: %s != %s", resa.Data[i].Blob, resb.Data[i].Blob)
+			}
+		}
 	}
 }
 
