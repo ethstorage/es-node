91c5ef4a3af95fa0b92c4aebe32712c29610de10
CobraLin <939255716@qq.com>
2024-04-20 13:54:43 +0800

    Fix typo

--- ethstorage/storage_manager.go
+++ ethstorage/storage_manager.go
@@ -93,7 +93,7 @@ func (s *StorageManager) DownloadFinished(newL1 int64, kvIndices []uint64, blobs
 			var err error = nil
 			for _, idx := range insertIdx {
 				c := prepareCommit(commits[idx])
-				// if return false, just ignore because we are not intersted in it
+				// if return false, just ignore because we are not interested in it
 				_, err = s.shardManager.TryWrite(kvIndices[idx], blobs[idx], c)
 				if err != nil {
 					break
