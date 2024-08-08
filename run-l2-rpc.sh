a3dde17dce8e62fa25756558395e88da79874f5c
syntrust <lundeng@quarkchain.org>
2024-08-08 10:59:52 +0800

    minor

--- run-l2-rpc.sh
+++ run-l2-rpc.sh
@@ -2,5 +2,4 @@
 
 ./run-l2.sh \
   --rpc.addr 0.0.0.0 \
-  --miner.enabled=false \
-  --p2p.bootnodes enr:-Li4QA-fcxSHHu68uzHxsGnR8Q8lnvPir8L3cb5-RSq5fvU7cmxukZinZ9N-XRcnvWauQl6KK2tnlD3RZTwOxI4KgIaGAZC-hjTfimV0aHN0b3JhZ2XbAYDY15RkADrb3zAU9-OPxr51LrBHuV2omsGAgmlkgnY0gmlwhEFtcySJc2VjcDI1NmsxoQNY8raIsHIGPniQ738UiNmIvifax5L6R51YLPoflGzix4N0Y3CCJAaDdWRwgnZh \
+  --miner.enabled=false
--- run-rpc.sh
+++ run-rpc.sh
@@ -2,5 +2,4 @@
 
 ./run.sh \
   --rpc.addr 0.0.0.0 \
-  --miner.enabled=false \
-  --p2p.bootnodes enr:-Li4QFpDtIlnf02Bli8jnZEkVAFyWkOOtaUZL7yKp3ySKmhGNiqRSe4AuUcFip3F4o_YLh30HJUg2UlcmIxx5W-fsK2GAY1eoPcdimV0aHN0b3JhZ2XbAYDY15SATFINPAhMgF43o16QBXrDKDH5b8GAgmlkgnY0gmlwhEFtMpGJc2VjcDI1NmsxoQL0mXwUXANkLHIAjN23dPfnOOhu-jhFUN13jcjHWeIP04N0Y3CCJAaDdWRwgnZh
+  --miner.enabled=false
