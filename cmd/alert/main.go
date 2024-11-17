10619fa8948bf47cea6a813142ca3046c77262b0
pingke <pingke@quarkchain.org>
2024-11-17 19:16:08 +0800

    add checkers

--- cmd/alert/main.go
+++ cmd/alert/main.go
@@ -1,78 +1,169 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethstorage/go-ethstorage/ethstorage/eth"
-	"github.com/ethstorage/go-ethstorage/ethstorage/miner"
 )
 
 const (
-	emailFormat              = "<html><body><div><h3>Ethstorage Alert!</h3>%s</div></body></html>"
-	noMinedBlockAlertContent = "<p>No blocks mined in last 24 hours. Last mined block: %d; last mined time: %v; l1 source: %s.</p>"
-	errorContent             = "<p>Check alert fail with error: %s</p>"
+	emailFormat  = "<html><body><div><h2>EthStorage Alert!</h2>%s</div></body></html>"
+	errorContent = "<p><b>Alert %s: </b>Check alert fail with error: %s</p>"
 )
 
 var (
-	rpcURLFlag     = flag.String("rpcurl", "http://88.99.30.186:8545", "L1 RPC URL")
-	l1ContractFlag = flag.String("l1contract", "0x804C520d3c084C805E37A35E90057Ac32831F96f", "Storage contract address on l1")
-	bodyFileFlag   = flag.String("htmlbody", "body.html", "Alert email html body file")
+	ruleFileFlag = flag.String("rules", "rules.json", "File contain the rules need to check")
+	bodyFileFlag = flag.String("htmlbody", "body.html", "Alert email html body file")
 )
+
+type IChecker interface {
+	Check(logger log.Logger) (bool, string)
+}
+
+type AlertConfig struct {
+	AlertType string            `json:"alert-type"`
+	Params    map[string]string `json:"params"`
+}
+
+func LoadConfig(ruleFile string) []IChecker {
+	file, err := os.Open(ruleFile)
+	if err != nil {
+		log.Crit("Failed to load rule file", "rule file", ruleFile, "err", err)
+	}
+	defer file.Close()
+
+	var alerts []*AlertConfig
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&alerts); err != nil {
+		log.Crit("Failed to decode rule file", "rule file", *ruleFileFlag, "err", err)
+	}
+
+	checkers := make([]IChecker, 0)
+	for _, alert := range alerts {
+		switch {
+		case alert.AlertType == "ESLastMinedBlockChecker":
+			checker, err := newESLastMinedBlockChecker(alert.Params)
+			if err != nil {
+				log.Crit("Failed to load ESLastMinedBlockChecker", "params", alert.Params, "err", err)
+			}
+			checkers = append(checkers, checker)
+		case alert.AlertType == "LastBlockChecker":
+			checker, err := newLastBlockChecker(alert.Params)
+			if err != nil {
+				log.Crit("Failed to load LastBlockChecker", "params", alert.Params, "err", err)
+			}
+			checkers = append(checkers, checker)
+		case alert.AlertType == "WebsiteOnlineChecker":
+			checker, err := newWebsiteOnlineChecker(alert.Params)
+			if err != nil {
+				log.Crit("Failed to load WebsiteOnlineChecker", "params", alert.Params, "err", err)
+			}
+			checkers = append(checkers, checker)
+		default:
+			log.Crit("Failed to load alert with unknown type", "type", alert.AlertType, "params", alert.Params)
+		}
+	}
+
+	return checkers
+}
+
+/*
+func main() {
+	flag.Parse()
+	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(3), log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
+
+	file, err := os.Create(*ruleFileFlag)
+	if err != nil {
+		log.Crit("Failed to load rule file", "rule file", *ruleFileFlag, "err", err)
+	}
+	defer file.Close()
+
+	cs := []*AlertConfig{
+		&AlertConfig{
+			AlertType: "ESLastMinedBlockChecker",
+			Params: map[string]string{
+				"Name":     "L2 Last Mined Block Alert",
+				"Contract": "0x804C520d3c084C805E37A35E90057Ac32831F96f",
+				"RPC":      "http://88.99.30.186:8545",
+			},
+		},
+		&AlertConfig{
+			AlertType: "LastBlockChecker",
+			Params: map[string]string{
+				"Name": "Galileo Last Block Alert",
+				"RPC":  "https://galileo.web3q.io:8545",
+			},
+		},
+		&AlertConfig{
+			AlertType: "WebsiteOnlineChecker",
+			Params: map[string]string{
+				"Name": "Galileo explorer online Alert",
+				"URL":  "https://explorer.galileo.web3q.io/",
+			},
+		},
+	}
+	//
+	// checkers := make([]IChecker, 0)
+	// checkers = append(checkers, &ESLastMinedBlockChecker{
+	// 	Name:     "L2 Last Mined Block Alert",
+	// 	Contract: common.HexToAddress("0x804C520d3c084C805E37A35E90057Ac32831F96f"),
+	// 	RPC:      "http://88.99.30.186:8545",
+	// })
+	// checkers = append(checkers, &LastBlockChecker{
+	// 	Name: "Galileo Last Block Alert",
+	// 	RPC:  "https://galileo.web3q.io:8545",
+	// })
+	// checkers = append(checkers, &WebsiteOnlineChecker{
+	// 	Name: "Galileo explorer online Alert",
+	// 	URL:  "https://explorer.galileo.web3q.io/",
+	// })
+
+	// decoder := json.NewDecoder(file)
+	// if err := decoder.Decode(&checkers); err != nil {
+	// 	log.Crit("Failed to decode rule file", "rule file", *ruleFileFlag, "err", err)
+	// }
+
+	// Encode data into JSON and write to the file
+	// encoder := json.NewEncoder(file)
+	// if err := encoder.Encode(checkers); err != nil {
+	// 	fmt.Println("Error encoding JSON:", err)
+	// 	return
+	// }
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(cs); err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+}*/
 
 func main() {
 	flag.Parse()
 	log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(3), log.StreamHandler(os.Stderr, log.TerminalFormat(true))))
 
 	var (
-		contract = common.HexToAddress(*l1ContractFlag)
-		logger   = log.New("app", "alert")
+		logger    = log.New("app", "alert")
+		needAlert = false
+		contents  = ""
 	)
 
-	client, err := eth.Dial(*rpcURLFlag, contract, 12, logger)
-	if err != nil {
-		log.Crit("Failed to create L1 source", "err", err)
+	checkers := LoadConfig(*ruleFileFlag)
+
+	for _, checker := range checkers {
+		res, content := checker.Check(logger)
+		if res {
+			needAlert = res
+			contents = contents + content + "\n"
+		}
 	}
 
-	needAlert, content := checkLastMinedBlock(client, contract, logger)
 	if needAlert {
-		writeHtmlFile(fmt.Sprintf(emailFormat, content))
+		writeHtmlFile(fmt.Sprintf(emailFormat, contents))
 		os.Exit(1)
 	}
-}
-
-func checkLastMinedBlock(client *eth.PollingClient, contract common.Address, logger log.Logger) (bool, string) {
-	var (
-		ctx = context.Background()
-		api = miner.NewL1MiningAPI(client, nil, logger)
-		err error
-	)
-
-	for i := 0; i < 3; i++ {
-		info, e := api.GetMiningInfo(ctx, contract, 0)
-		if e != nil {
-			time.Sleep(time.Minute)
-			log.Error("Get mining info fail", "error", e)
-			err = e
-			continue
-		}
-
-		lastMinedTime := time.Unix(int64(info.LastMineTime), 0)
-		logger.Info("", "last mined time", lastMinedTime, "mined block", info.BlockMined)
-		targetTime := time.Now().Add(-24 * time.Hour)
-		if targetTime.After(lastMinedTime) {
-			content := fmt.Sprintf(noMinedBlockAlertContent, info.BlockMined, lastMinedTime, *rpcURLFlag)
-			return true, content
-		}
-		return false, ""
-	}
-
-	return true, fmt.Sprintf(errorContent, err.Error())
 }
 
 func writeHtmlFile(content string) {
---
+++ cmd/alert/siteonlinechecker.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/pkg/errors"
+)
+
+const (
+	websiteOfflineAlertContent = "<p><b>Alert %s: </b>Web site is off line. RPC: %s.</p>"
+)
+
+type WebsiteOnlineChecker struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+func newWebsiteOnlineChecker(params map[string]string) (*WebsiteOnlineChecker, error) {
+	name, url := params["name"], params["url"]
+	if name == "" || url == "" {
+		return nil, errors.New("invalid params to load WebsiteOnlineChecker")
+	}
+
+	return &WebsiteOnlineChecker{
+		Name: name,
+		URL:  url,
+	}, nil
+}
+
+func (c *WebsiteOnlineChecker) Check(logger log.Logger) (bool, string) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Get(c.URL)
+	if err != nil {
+		logger.Error("Failed to request web site", "alert", c.Name, "url", c.URL, "err", err)
+		return true, fmt.Sprintf(errorContent, c.Name, err.Error())
+	}
+	defer resp.Body.Close()
+
+	logger.Info("Check last block", "alert", c.Name, "url", c.URL, "status", resp.StatusCode)
+	// Check the HTTP status code
+	if resp.StatusCode == http.StatusOK {
+		return false, ""
+	} else {
+		return true, fmt.Sprintf(websiteOfflineAlertContent, c.Name, c.URL)
+	}
+}
