e72e454fef5e5bf8e70372a15861a5dc5ce61d6e
syntrust <lundeng@quarkchain.org>
2024-12-25 17:17:31 +0800

    fix missing slash

--- run-l2.sh
+++ run-l2.sh
@@ -6,5 +6,5 @@
   --l1.block_time 2 \
   --da.url http://5.9.87.214:8888 \
   --randao.url http://88.99.30.186:8545 \
-  --p2p.bootnodes enr:-Li4QFWB-bdjo4EVuvJmJ4GmMuwa0RcFdtdpA6yUxTdU0QE8aG_q8eZa9B4vu2LGPTbspqFtMBCuJpjG0q178SY7H1yGAZP7b35dimV0aHN0b3JhZ2XbAYDY15RkADrb3zAU9-OPxr51LrBHuV2omsGAgmlkgnY0gmlwhEFtMpGJc2VjcDI1NmsxoQMw4i1lk4EjCD_4os-PSrC9y1BrDim_olfCCPNBoXSkWIN0Y3CCJAaDdWRwgnZh
+  --p2p.bootnodes enr:-Li4QFWB-bdjo4EVuvJmJ4GmMuwa0RcFdtdpA6yUxTdU0QE8aG_q8eZa9B4vu2LGPTbspqFtMBCuJpjG0q178SY7H1yGAZP7b35dimV0aHN0b3JhZ2XbAYDY15RkADrb3zAU9-OPxr51LrBHuV2omsGAgmlkgnY0gmlwhEFtMpGJc2VjcDI1NmsxoQMw4i1lk4EjCD_4os-PSrC9y1BrDim_olfCCPNBoXSkWIN0Y3CCJAaDdWRwgnZh \
  $@
