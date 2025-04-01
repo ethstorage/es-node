fad8c1e9e701a9de7d0fc4ac88e6a602ba8d92c4
syntrust <lundeng@quarkchain.org>
2025-04-01 17:53:41 +0800

    improve log

--- ethstorage/scanner/worker.go
+++ ethstorage/scanner/worker.go
@@ -31,16 +31,16 @@ func NewWorker(sr StorageReader, cr es.Il1Source, lg log.Logger) *Worker {
 }
 
 func (s *Worker) ScanBatch(ctx context.Context) error {
-	kvIndexRange, nextKvIndex, err := s.getBatchIndexRange()
+	kvsInBatch, nextKvIndex, err := s.determineBatchIndexRange()
 	if err != nil {
 		return fmt.Errorf("failed to get batch index range: %w", err)
 	}
-	metas, err := s.contractReader.GetKvMetas(kvIndexRange, rpc.LatestBlockNumber.Int64())
+	metas, err := s.contractReader.GetKvMetas(kvsInBatch, rpc.LatestBlockNumber.Int64())
 	if err != nil {
-		s.lg.Error("Error getting meta range", "kvIndexRange", kvIndexRange)
+		s.lg.Error("Scanner: error getting meta range", "kvsInBatch", shortPrt(kvsInBatch))
 		return fmt.Errorf("failed to query kv metas %w", err)
 	}
-	s.lg.Info("Query KV meta done", "kvIndexRange", kvIndexRange, "metaLen", len(metas))
+	s.lg.Info("Scanner: query KV meta done", "kvsInBatch", shortPrt(kvsInBatch), "metaLen", len(metas))
 	for k, meta := range metas {
 		select {
 		case <-ctx.Done():
@@ -52,32 +52,33 @@ func (s *Worker) ScanBatch(ctx context.Context) error {
 		var commit common.Hash
 		copy(commit[:], meta[32-es.HashSizeInContract:32])
 		// query blob and check commit
-		_, found, err := s.storageReader.TryRead(kvIndexRange[k], int(s.storageReader.MaxKvSize()), commit)
-		if err != nil {
-			s.lg.Error("Read blob error", "err", err)
-			// TODO fix invalid kv
-		}
+		_, found, err := s.storageReader.TryRead(kvsInBatch[k], int(s.storageReader.MaxKvSize()), commit)
+
 		if !found {
-			s.lg.Warn("Blob not found", "kvIndex", kvIndexRange[k], "blobHash", commit)
+			s.lg.Warn("Scanner: blob not found", "kvIndex", kvsInBatch[k], "blobHash", commit)
+		} else if err != nil {
+			s.lg.Error("Scanner: read blob error", "kvIndex", kvsInBatch[k], "blobHash", fmt.Sprintf("0x%x", commit), "err", err)
+			// TODO fix invalid kv
+		} else {
+			s.lg.Debug("Scanner: check KV done", "kvIndex", kvsInBatch[k], "blobHash", commit)
 		}
-		s.lg.Info("Check KV done", "kvIndex", kvIndexRange[k], "blobHash", commit)
 	}
 	s.nextKvIndex = nextKvIndex
 	return nil
 }
 
-func (s *Worker) getBatchIndexRange() ([]uint64, uint64, error) {
-	localKvs, err := s.getLocalKvs()
+func (s *Worker) determineBatchIndexRange() ([]uint64, uint64, error) {
+	localKvs, err := s.queryLocalKvs()
 	if err != nil {
 		return nil, 0, fmt.Errorf("failed to get local kv indices: %w", err)
 	}
-	s.lg.Info("Query local kv done", "localKVs", localKvs)
+	s.lg.Info("Scanner: query local kv done", "localKVs", shortPrt(localKvs))
 	localKvTotal := uint64(len(localKvs))
 
 	batchStart := s.nextKvIndex
 	if batchStart == localKvTotal {
 		batchStart = 0
-		s.lg.Info("Scan batch start over")
+		s.lg.Info("Scanner: scan batch start over")
 	}
 	batchEnd := batchStart + MetaDownloadBatchSize
 	if batchEnd > localKvTotal {
@@ -86,12 +87,12 @@ func (s *Worker) getBatchIndexRange() ([]uint64, uint64, error) {
 	return localKvs[batchStart:batchEnd], batchEnd, nil
 }
 
-func (s *Worker) getLocalKvs() ([]uint64, error) {
+func (s *Worker) queryLocalKvs() ([]uint64, error) {
 	total, err := s.contractReader.GetStorageLastBlobIdx(rpc.LatestBlockNumber.Int64())
 	if err != nil {
 		return nil, fmt.Errorf("failed to query total kv entries: %w", err)
 	}
-	s.lg.Info("Query total kv entries done", "total", total)
+	s.lg.Info("Scanner: query total kv entries done", "kvEntryCount", total)
 	var localKvs []uint64
 	kvEntries := s.storageReader.KvEntries()
 	localShards := s.storageReader.Shards()
@@ -103,9 +104,16 @@ func (s *Worker) getLocalKvs() ([]uint64, error) {
 			kvIdx := shardId*kvEntries + k
 			if kvIdx < total {
 				// only check non-empty data
-				localKvs = append(localKvs, shardId*kvEntries+k)
+				localKvs = append(localKvs, kvIdx)
 			}
 		}
 	}
 	return localKvs, nil
+}
+
+func shortPrt(arr []uint64) string {
+	if len(arr) <= 6 {
+		return fmt.Sprintf("%v", arr)
+	}
+	return fmt.Sprintf("%v ... %v\n", arr[:3], arr[len(arr)-3:])
 }
