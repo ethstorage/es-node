e7ea794c3e1b7a3b558bd607222f9c10660a37d2
qiang <freedostudio@gmail.com>
2025-05-22 15:42:37 +0800

    ethstorage l2 boot node change

--- run-l2.sh
+++ run-l2.sh
@@ -7,5 +7,5 @@
   --l2.chain_id 3337 \
   --da.url http://5.9.87.214:8888 \
   --randao.url http://88.99.30.186:8545 \
-  --p2p.bootnodes enr:-Lq4QJntX8Wl0CDHg3z32mcILXLiplkRIJnCJynEmGTgEJ4XVjNsum7qQsndJsfc4h_5lWwVU-Nq-Hs3A-JIoO8KVfiGAZP7b4KIimV0aHN0b3JhZ2Xdgg0JgNjXlGQAOtvfMBT344_GvnUusEe5XaiawYCCaWSCdjSCaXCEQW0ykYlzZWNwMjU2azGhAzDiLWWTgSMIP_iiz49KsL3LUGsOKb-iV8II80GhdKRYg3RjcIIkBoN1ZHCCdmE \
+  --p2p.bootnodes enr:-Lq4QEXYjk9uxm2_r-JXoXnpqMu54YIIrTFYd7sfQKAgFQL1fiq4mkUHGkV6mIas6sh7klavj5z6sFZYRfvtldSq6LOGAZb2MuHbimV0aHN0b3JhZ2Xdgg0JgNjXlGQAOtvfMBT344_GvnUusEe5XaiawYCCaWSCdjSCaXCEisl6PYlzZWNwMjU2azGhAgsmoUh0dDjPTG5kiyxCDwG5VNK_d1WCSJ4KkVJ6-57zg3RjcIIkBoN1ZHCCdmE \
  $@
