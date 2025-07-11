9a0a3da70c6d4cf4adb313a4e339935d481c8a42
DL <lundeng@quarkchain.org>
2025-07-11 19:14:08 +0800

    check miner role (#407)
    * check miner role
    
    * minor
    
    * check contract version

--- ethstorage/node/node.go
+++ ethstorage/node/node.go
@@ -309,6 +309,9 @@ func (n *EsNode) initMiner(ctx context.Context, cfg *Config) error {
 		return nil
 	}
 	l1api := miner.NewL1MiningAPI(n.l1Source, n.randaoSource, n.log)
+	if err := l1api.CheckMinerRole(ctx, cfg.Storage.L1Contract, cfg.Storage.Miner); err != nil {
+		n.log.Crit("Check miner role", "err", err)
+	}
 	pvr := prover.NewKZGPoseidonProver(
 		cfg.Mining.ZKWorkingDir,
 		cfg.Mining.ZKeyFile,
