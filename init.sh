97e222519d1e44a33f58638d6e9d7ef09bd3f1f5
ping-ke <molaokp@gmail.com>
2025-07-24 22:14:58 +0800

    verify new zkey using it

--- init.sh
+++ init.sh
@@ -55,12 +55,12 @@ if [ $use_miner = 1 ]; then
   mkdir -p ./build/bin/snark_lib/zkey
   # download zkey if not yet
   zkey_name="blob_poseidon2.zkey"
-  zkey_size=560301223
-  zkey_url="https://es-zkey.s3.us-west-2.amazonaws.com/blob_poseidon2.zkey"
+  zkey_size=560303266
+  zkey_url="https://ethstorage-trusted-setup.s3.us-west-2.amazonaws.com/blob-poseidon-2-circuit_final.zkey"
   if [ "$zkp_mode" = 1 ]; then
     zkey_name="blob_poseidon.zkey"
-    zkey_size=280151245
-    zkey_url="https://es-zkey.s3.us-west-2.amazonaws.com/blob_poseidon1.zkey"
+    zkey_size=280153702
+    zkey_url="https://ethstorage-trusted-setup.s3.us-west-2.amazonaws.com/blob-poseidon-circuit_final.zkey"
   fi
   zkey_file="./build/bin/snark_lib/zkey/$zkey_name"
   if [ ! -e  ${zkey_file} ] || [ $(wc -c <  ${zkey_file}) -ne ${zkey_size} ]; then
