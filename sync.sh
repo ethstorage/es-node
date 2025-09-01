86c34bc7cb4146a1dd603d43766c509a849e5e32
Qiang Zhu <freedostudio@gmail.com>
2025-09-01 15:24:36 +0800

    2nd testnet boot (#432)

--- sync.sh
+++ sync.sh
@@ -16,7 +16,7 @@ done
 start_flags=" sync \
   --datadir $data_dir \
   $file_flags \
-  --storage.l1contract 0x804C520d3c084C805E37A35E90057Ac32831F96f \
+  --storage.l1contract 0xAb3d380A268d088BA21Eb313c1C23F3BEC5cfe93 \
   --l1.rpc http://65.108.230.142:8545 \
   --es_rpc http://65.108.230.142:9545 \
   $@"
