4df2877a3dd9b7c4e02b7119dc3023ae8fc7da24
syntrust <lundeng@quarkchain.org>
2025-09-08 18:32:13 +0800

    mainnet scripts

---
+++ init-mainnet.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./init.sh \
+  --l1.rpc http://65.108.230.142:8550 \
+  --storage.l1contract <> \
+$@
---
+++ run-mainnet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./run.sh \
+  --chain_id 333 \
+  --storage.l1contract <> \
+  --l1.rpc http://65.108.230.142:8550 \
+  --p2p.bootnodes <> \
+  $@
