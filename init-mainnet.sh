7cc65cabedf672104233f46d252a9eae16b32fa2
syntrust <lundeng@quarkchain.org>
2025-09-10 15:54:03 +0800

    check rpc

--- init-mainnet.sh
+++ init-mainnet.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+has_l1_rpc=0
+for arg in "$@"; do
+  if [[ "$arg" == "--l1.rpc" ]] || [[ "$arg" == --l1.rpc=* ]]; then
+    has_l1_rpc=1
+    break
+  fi
+done
+
+# Exit to avoid using testnet RPC in init.sh if --l1.rpc is not provided
+if [[ $has_l1_rpc -eq 0 ]]; then
+  echo "Error: --l1.rpc is required for mainnet initialization"
+  echo "Usage: $0 --l1.rpc <mainnet_rpc_url> [other_options]"
+  exit 1
+fi
+
 ./init.sh \
   --storage.l1contract <> \
-$@
+  "$@"
