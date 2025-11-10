4d766ce790050c6110c247afd8ba0594263cdb55
syntrust <lundeng@quarkchain.org>
2025-11-10 15:07:29 +0800

    apply Fusaka beacon api to download blobs

--- ethstorage/archiver/service.go
+++ ethstorage/archiver/service.go
@@ -35,7 +35,7 @@ type APIService struct {
 }
 
 // blobSidecarHandler implements the /eth/v1/beacon/blob_sidecars/{id} endpoint,
-// but allows clients to fetch expired blobs.
+// deprecated since Fusaka
 func (a *APIService) blobSidecarHandler(w http.ResponseWriter, r *http.Request) {
 
 	a.lg.Info("Blob archiver API request", "from", readUserIP(r), "url", r.RequestURI)
@@ -73,6 +73,44 @@ func (a *APIService) blobSidecarHandler(w http.ResponseWriter, r *http.Request) 
 	}
 }
 
+// blobSidecarHandler implements the /eth/v1/beacon/blobs/{id} endpoint
+func (a *APIService) blobsHandler(w http.ResponseWriter, r *http.Request) {
+
+	a.lg.Info("Blob archiver API request", "from", readUserIP(r), "url", r.RequestURI)
+
+	id := mux.Vars(r)["id"]
+	if hErr := validateBlockID(id); hErr != nil {
+		hErr.write(w)
+		return
+	}
+
+	hashes, hErr := parseVersionedHashes(r)
+	if hErr != nil {
+		hErr.write(w)
+		return
+	}
+
+	result, hErr := a.api.queryBlobs(id, hashes)
+	if hErr != nil {
+		hErr.write(w)
+		return
+	}
+
+	if len(result.Data) == 0 {
+		a.lg.Info("Not stored by EthStorage", "beaconID", id)
+		errBlobNotInES.write(w)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	encodingErr := json.NewEncoder(w).Encode(result)
+	if encodingErr != nil {
+		a.lg.Error("Unable to encode blob sidecars to JSON", "err", encodingErr)
+		errServerError.write(w)
+		return
+	}
+}
+
 func (a *APIService) Start(ctx context.Context) error {
 	a.lg.Debug("Starting blob archiver API service", "address", a.cfg.ListenAddr)
 	endpoint := net.JoinHostPort(a.cfg.ListenAddr, strconv.Itoa(a.cfg.ListenPort))
@@ -81,7 +119,10 @@ func (a *APIService) Start(ctx context.Context) error {
 		return err
 	}
 	r := mux.NewRouter()
+	// Deprecated
 	r.HandleFunc("/eth/v1/beacon/blob_sidecars/{id}", a.blobSidecarHandler)
+	// Fusaka
+	r.HandleFunc("/eth/v1/beacon/blobs/{id}", a.blobsHandler)
 
 	a.apiServer = httputil.NewHttpServer(r)
 	go func() {
