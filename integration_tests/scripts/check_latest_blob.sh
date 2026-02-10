7523dfcbd11817548cc171aca13b771cc4df159f
syntrust <lundeng@quarkchain.org>
2026-02-10 10:33:45 +0800

    extend buffer

--- integration_tests/scripts/check_latest_blob.sh
+++ integration_tests/scripts/check_latest_blob.sh
@@ -36,7 +36,7 @@ echo "Failed to get latest block" >&2
 exit 1
 fi
 
-latest_dec=$((latest_dec - 64 - 15))  # adjust to finalized, and add some buffer for blob downloading
+latest_dec=$((latest_dec - 64 - 32))  # adjust to finalized, and add some buffer (1 epoch) for blob downloading to disk
 from_dec=$((latest_dec - lookback))
 from_hex="$(cast to-hex "$from_dec")"
 to_hex="$(cast to-hex "$latest_dec")"
