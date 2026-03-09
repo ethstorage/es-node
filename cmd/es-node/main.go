a76d63cac9d974341f31f0ee768c5989190a1775
syntrust <lundeng@quarkchain.org>
2026-03-09 15:36:23 +0800

    bump version to v0.2.9

--- cmd/es-node/main.go
+++ cmd/es-node/main.go
@@ -32,7 +32,7 @@ import (
 var (
 	GitCommit     = ""
 	GitDate       = ""
-	Version       = "v0.2.8"
+	Version       = "v0.2.9"
 	Meta          = "dev"
 	BuildTime     = ""
 	systemVersion = fmt.Sprintf("%s/%s", runtime.GOARCH, runtime.GOOS)
